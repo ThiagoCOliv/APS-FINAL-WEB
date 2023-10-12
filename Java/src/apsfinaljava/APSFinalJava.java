140876e40656547a9b20730ace1fe07f4ef3198d
Thiago Correa <thiagocdo445@gmail.com>
2023-10-12 13:38:29 -0300

    Adição da parte Java Desktop

---
+++ Java/src/apsfinaljava/APSFinalJava.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package apsfinaljava;
+
+import apresentação.FrmCidade;
+
+/**
+ *
+ * @author thiag
+ */
+public class APSFinalJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmCidade formCidade = new FrmCidade(null, true);
+        formCidade.setVisible(true);
+    }
+    
+}
